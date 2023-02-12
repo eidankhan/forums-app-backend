c07c0b8f768cc989fea1d1d90cc52698735e65f7
Eidan Khan <eidan.khan659@gmail.com>
2023-02-12 15:53:34 +0500

    init

---
+++ src/main/java/com/forums/app/ForumsAppApplication.java
@@ -0,0 +1,13 @@
+package com.forums.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumsAppApplication.class, args);
+	}
+
+}
